801656d606df65e888a404db8eebe56dbc78131a
jymsy <jym20083617@163.com>
2013-09-18 11:01:14 +0800

    delete crequest

--- crequest/admin/view/return_json.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo json_encode(array('resultCode'=>$resultCode, 'resultMsg'=>$resultMsg,'data'=>$data));
--- crequest/install.php
+++
@@ -1,7 +0,0 @@
-<?php
-chdir(__DIR__);
-if (!is_dir('log')) mkdir('log', 0777);
-chmod('log', 0777);
-if (!is_dir('config')) mkdir('config', 0777);
-chmod('config', 0777);
-echo 'Success create folder: config, log',PHP_EOL;
